4ddb011d3ccc2137b047831053bbe7bed32619cf
zichenma <mazichen1984@gmail.com>
2017-05-25 10:16:29 -0400

    new apis added
    not working: 
    missing_titles_age_threshold
    open_complaints
    repo_vendor_status
    suggested_collections_actions

---
+++ api.js
@@ -0,0 +1,49 @@
+//this file contains all apis data
+
+var apis = {
+    "Call Vol Over Time": 
+    "http://apis.firsthelpfinancial.com/ords/collectionreports/call_vol_ovrtme_by_collctr/",
+
+    "Current Repo Status Summary": 
+    "http://apis.firsthelpfinancial.com/ords/collectionreports/currnt_repo_status_summry/",
+
+    "Current Active Repo Assignments": 
+    "http://apis.firsthelpfinancial.com/ords/collectionreports/currnt_activ_repo_asgnmnt/",
+
+    "Funding GPS": 
+    "http://apis.firsthelpfinancial.com/ords/collectionreports/funding_gps/",
+    
+    "Liquidation Report": 
+    "http://apis.firsthelpfinancial.com/ords/collectionreports/liquidation_report/",
+
+    "Missing Titles Age Thres hold": 
+    "http://apis.firsthelpfinancial.com/ords/collectionreports/missing_titles_age_threshold/",
+    
+    "Misssing Titles Reports": 
+    "http://apis.firsthelpfinancial.com/ords/collectionreports/missing_titles_report/",
+    
+    "Open Complaints": 
+    "http://apis.firsthelpfinancial.com/ords/collectionreports/open_complaints/",
+     
+    "Repo Vendor Status": 
+    "http://apis.firsthelpfinancial.com/ords/collectionreports/repo_vendor_status/",
+
+    "Repo Status Summary": 
+    "http://apis.firsthelpfinancial.com/ords/collectionreports/currnt_repo_status_summry/",
+    
+    "Charge off List": 
+    "http://apis.firsthelpfinancial.com/ords/collectionreports/charge_offs_list/",
+
+    "Change Offs By State Overtime":
+    "http://apis.firsthelpfinancial.com/ords/collectionreports/chrge_offs_by_state_ovrtme/",
+
+    "Coll Call Counts By Week":
+    "http://apis.firsthelpfinancial.com/ords/collectionreports/coll_call_counts_by_wk/",
+
+    "Complaint Status Changes":
+    "http://apis.firsthelpfinancial.com/ords/collectionreports/complaint_status_changes/",
+
+    "Suggested Collections Actions List": 
+    "http://apis.firsthelpfinancial.com/ords/collectionreports/suggested_collections_actions/"
+}
+
